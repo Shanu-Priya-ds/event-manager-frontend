f961e0e48fc204a1627af1710b4fe4328fd84a8f
Shanu Priya <priyads12@gmail.com>
2026-06-04 11:01:34 -0400

    installed toaster package and imported to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast';
 import './App.css';
 import Welcome from "./pages/Welcome";
 import Register from "./pages/Register";
@@ -9,15 +10,18 @@ import Dashboard from './pages/Dashboard';
 import EventDetails from './pages/EventDetails';
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Welcome/>} />
-      <Route path="/register" element={<Register/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/myRegistrations" element={<MyRegistrations/>}/>
-      <Route path="/dashboard" element={<Dashboard></Dashboard>}/>
-      <Route path="/eventDetails/:id" element={<EventDetails/>}/>
-   </Routes>
+    <>
+      <Toaster position="bottom-right" />
+      <Routes>
+        <Route path="/" element={<Welcome/>} />
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/myRegistrations" element={<MyRegistrations/>}/>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}/>
+        <Route path="/eventDetails/:id" element={<EventDetails/>}/>
+      </Routes>
+    </>
   )
 }
 
