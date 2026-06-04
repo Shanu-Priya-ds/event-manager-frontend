738118bfbd27f2e788ad4323ee1248450a671393
Shanu Priya <priyads12@gmail.com>
2026-06-04 17:20:40 -0400

    Added Protected Route

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import MyRegistrations from './pages/MyRegistrations';
 import Dashboard from './pages/Dashboard';
 import EventDetails from './pages/EventDetails';
 import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
 function App() {
   return (
     <>
@@ -18,10 +19,13 @@ function App() {
         <Route path="/" element={<Welcome/>} />
         <Route path="/register" element={<Register/>}/>
         <Route path="/login" element={<Login/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/myRegistrations" element={<MyRegistrations/>}/>
-        <Route path="/dashboard" element={<Dashboard></Dashboard>}/>
-        <Route path="/eventDetails/:id" element={<EventDetails/>}/>
+          
+        <Route element={<ProtectedRoute/>}>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/myRegistrations" element={<MyRegistrations/>}/>
+          <Route path="/dashboard" element={<Dashboard></Dashboard>}/>
+          <Route path="/eventDetails/:id" element={<EventDetails/>}/>
+        </Route>
       </Routes>
     </>
   )
