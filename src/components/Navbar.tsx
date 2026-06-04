3b28fb3c83378ef8f037565b0210062eb2a35a1d
Shanu Priya <priyads12@gmail.com>
2026-06-04 19:50:37 -0400

    added styles to login and navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,13 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 function Navbar(){
+
+    const { pathname } = useLocation();
+
+    const pagesToHide =["/login","/register"];
+    //hide navbar for login and register
+    if(pagesToHide.includes(pathname)) return null;
 
     const navigate = useNavigate();
     const {logout, user} = useAuth();
@@ -17,32 +23,32 @@ function Navbar(){
         <nav className="bg-gradient-to-r from-grey-600 to-grey-700 text-black p-2 shadow-lg">
             <div className="flex justify-between items-center">
                 <h5 className="text-l font-bold">Event Management System</h5>
-                <div className="flex gap-6">
+                <div className="flex gap-2">
                     <button
                         onClick={()=> navigate("/home")}
-                        className="hover:bg-gray-300 px-4 py-2 cursor-pointer rounded transition-colors duration-200"
+                        className="hover:bg-gray-300 text-xs p-1 cursor-pointer rounded transition-colors duration-200"
                     >
                         Home
                     </button>
                     <button
                         onClick={()=> navigate("/dashboard")}
-                        className="hover:bg-gray-300 px-4 py-2 cursor-pointer cursor-pointer rounded transition-colors duration-200"
+                        className="hover:bg-gray-300 text-xs p-1 cursor-pointer cursor-pointer rounded transition-colors duration-200"
                     >
                         My events
                     </button>
                     <button
                         onClick={()=> navigate("/myRegistrations")}
-                        className="hover:bg-gray-300 px-2 py-1 rounded cursor-pointer transition-colors duration-100"
+                        className="hover:bg-gray-300 text-xs p-1 rounded cursor-pointer transition-colors duration-100"
                     >
                         My Registrations
                     </button>
                 </div>
                 {user ?
                     <div className="flex items-center gap-4">
-                        <p className="text-xs">Welcome {user.username}</p>
+                        <p className="text-xs font-semibold">Welcome {user.username}</p>
                         <button
                             onClick={handleLogOut}
-                            className="bg-gray-500 hover:bg-gray-600 cursor-pointer px-2 py-1 rounded transition-colors duration-200"
+                            className="bg-gray-300 text-xs hover:bg-gray-600 cursor-pointer  px-1 py-0.5 rounded transition-colors duration-200"
                         >
                             Logout
                         </button>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { useState, type ChangeEvent, type SyntheticEvent } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import useApi from "../hooks/useApi";
 import type { LoginResponse } from "../types/types";
@@ -17,7 +17,7 @@ function Login() {
         url: "/auth/login",
         method: "POST",
         autoFetch: false,
-        successMsg:"You are logged In now."
+        successMsg: "You are logged In now."
     });
 
     async function handleSubmit(e: SyntheticEvent) {
@@ -36,15 +36,40 @@ function Login() {
         setFormData(prev => ({ ...prev, [name]: value }))
     }
 
-    return (<>
-        <h1>Login Page</h1>
-        {error && <p style={{color: "red"}}>{error}</p>}
-        <form onSubmit={handleSubmit}>
-            <input name="email" value={formData.email} onChange={handleFormChange} type="text" placeholder="enter email" />
-            <input type="password" value={formData.password} onChange={handleFormChange} name="password" placeholder="enter password" />
-            <button type="submit" disabled={loading}>{loading ? "Logging in..." : "Login"}</button>
-        </form>
-    </>)
+    return (
+        <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+            <div className="flex-col p-6 rounded-xl">
+                <div className="text-center mb-2">
+                    <h1 className="text-xl font-semibold">Event Manager</h1>
+                    <p className="text-sm text-gray-500 mt-1">Sign in to your account</p>
+                </div>
+
+                <form className="bg-white border border-gray-200 rounded-xl p-6" onSubmit={handleSubmit}>
+                    <div className="mb-4 ">
+                         <label className="block text-sm font-medium mb-1.5">Email address</label>
+                        <input name="email" className="rounded-l border border-gray-200 px-1"
+                            value={formData.email} onChange={handleFormChange} type="text" placeholder="enter email" />
+
+                    </div>
+                    <div className="mb-5">
+                        <div className="flex justify-between mb-1.5"><label className="text-sm font-medium">Password</label>
+                            <a href="#" className="text-xs text-blue-600">Forgot password?</a>
+                        </div>
+                        <input type="password" value={formData.password}
+                            onChange={handleFormChange} name="password"
+                            className="rounded-l border border-gray-200 px-1" placeholder="enter password" />
+                    </div>
+                    <button type="submit" className="bg-gray-400 cursor-pointer w-full text-white px-1 py-1 rounded" disabled={loading}>{loading ? "Logging in..." : "Sign in"}</button>
+                </form>
+
+                <p className="text-center text-sm text-gray-500 mt-4">
+                    Don't have an account?{" "}
+                    <Link to="/register" className="text-blue-600 font-medium">Sign up</Link>
+                </p>
+
+            </div>
+        </div>
+    );
 }
 
 export default Login;
