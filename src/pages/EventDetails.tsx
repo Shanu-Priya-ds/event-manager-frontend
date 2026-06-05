b63123a581c7d32586e07a70986bbfa023187c7b
Shanu Priya <priyads12@gmail.com>
2026-06-05 01:02:08 -0400

    Modified styles in event details page, invoke registration api

--- src/pages/EventDetails.tsx
+++ src/pages/EventDetails.tsx
@@ -2,42 +2,73 @@ import { useParams } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import useApi from "../hooks/useApi";
 import type { EventModel } from "../types/types";
+import { Calendar, Clock, MapPin, User, Users } from "lucide-react";
 
 function EventDetails() {
     const { id } = useParams();
     const auth = useAuth();
 
-    const { data: event, loading, error } = useApi<EventModel>({
+    const { data: event, loading } = useApi<EventModel>({
         url: `/events/${id}`,
         method: "GET",
         autoFetch: true,
-        successMsg:""
+        successMsg: ""
     });
 
+    const { execute: register } = useApi<EventModel>({
+        url: `registrations/${id}`,
+        method: "POST",
+        autoFetch: false,
+        successMsg: `Successfully registered`
+    })
+
     if (loading) return <div><p>Loading event details...</p></div>;
-    if (error) return <div><p style={{ color: "red" }}>Error: {error}</p></div>;
+   // if (error) return <div><p style={{ color: "red" }}>Error: {error}</p></div>;
     if (!event || Array.isArray(event)) return <div><p>Event not found</p></div>;
 
     const isOrganizer = event.organizerId === auth.user?.userId;
     const isAttendee = event.attendees?.some((a: any) => a.userId === auth.user?.userId);
+     console.log(isAttendee)
+
+
+    function handleRegister() {
+        if (!isAttendee) {
+            register();
+        }
+    }
 
     return (
-        <div>
-            <h1>{event.title}</h1>
+        <div className="m-3">
+            <h1 className="pb-3 font-bold text-2xl">{event.title}</h1>
 
-            {event.imageUrl && <img src={event.imageUrl} alt={event.title} style={{ maxWidth: "100%", height: "auto" }} />}
+            {event.imageUrl && <img src={event.imageUrl} alt={event.title}
+                style={{ maxWidth: "100%", height: "auto" }} />}
+
+            <p className="p-3">{event.description}</p>
+
 
             <div>
-                <h3>Description</h3>
-                <p>{event.description}</p>
-            </div>
+                <div className="flex gap-1">
+                    <MapPin className="w-3 h-3 self-center" />
+                    <p> Venue: {event.venue}</p>
+                </div>
+                <div className="flex gap-1">
+                    <Calendar className="w-3 h-3 self-center" />
+                    <p> Date: {new Date(event.dateTime).toLocaleDateString()}</p>
+                </div>
+                <div className="flex gap-1">
+                    <Clock className="w-3 h-3 self-center" />
+                    <p> Time: {new Date(event.dateTime).toLocaleTimeString()}</p>
+                </div>
+                <div className="flex gap-1">
+                    <Users className="w-3 h-3 self-center" />
+                    <p> Capacity: {event.attendeeCount} / {event.capacity}</p>
+                </div>
+                <div className="flex gap-1">
+                    <User className="w-3 h-3 self-center" />
 
-            <div>
-                <p><strong>📍 Venue:</strong> {event.venue}</p>
-                <p><strong>📅 Date:</strong> {new Date(event.dateTime).toLocaleDateString()}</p>
-                <p><strong>🕐 Time:</strong> {new Date(event.dateTime).toLocaleTimeString()}</p>
-                <p><strong>👥 Capacity:</strong> {event.attendeeCount} / {event.capacity}</p>
-                <p><strong>👤 Organizer:</strong> {event.organizerName}</p>
+                    <p> Organizer: {event.organizerName}</p>
+                </div>
             </div>
 
             {isOrganizer ? (
@@ -58,7 +89,8 @@ function EventDetails() {
                     </div>
                 </>
             ) : (
-                <button style={{ backgroundColor: isAttendee ? "green" : "blue", color: "white" }}>
+                <button className=" m-2 bg-gray-300 cursor-pointer text-sm rounded-md px-2 py-0.5"
+                    onClick={handleRegister}>
                     {isAttendee ? "Registered ✓" : "Register"}
                 </button>
             )}
