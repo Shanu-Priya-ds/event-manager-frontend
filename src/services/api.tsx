b9339cfd4562c4290314100cc4c937bf8f847122
Shanu Priya <priyads12@gmail.com>
2026-06-05 02:35:09 -0400

    update baseURL with /api

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({baseURL:import.meta.env.VITE_API_BASE_URL});
+const axiosInstance = axios.create({baseURL:`${import.meta.env.VITE_API_BASE_URL}/api`});
 
 axiosInstance.interceptors.request.use((config)=>{
     const token = localStorage.getItem("token");
